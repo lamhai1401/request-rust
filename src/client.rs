bd038b167d5f49333ff11c451d2ce524c3fc1750
lamhai1401 <lamthanhhai141@gmail.com>
2022-04-11 16:59:29 +0700

    add generic resp for api

--- src/client.rs
+++ src/client.rs
@@ -1,12 +1,10 @@
 use super::{
     errors::Error,
-    rq::{ReqResult, Request, Resp},
+    rq::{ReqResult, Request},
 };
 use async_trait::async_trait;
 use reqwest::{header, Client, StatusCode};
-use serde_json::json;
-
-use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 
 pub struct Api {
     url: String,
@@ -16,6 +14,7 @@ pub struct Api {
 #[async_trait]
 impl Request for Api {
     const PATH: &'static str = "/api";
+    type Output = String;
     fn new(url: String) -> Self {
         Api {
             url,
@@ -23,19 +22,72 @@ impl Request for Api {
         }
     }
 
-    async fn get(&mut self, uri: String) -> ReqResult {
-        // let url = format!(
-        //     "{}{}{}",
-        //     self.url.clone(),
-        //     Self::PATH.clone(),
-        //     uri.to_owned()
-        // );
-        let resp = self.client.get(uri.to_owned()).send().await?.text().await?;
-        match serde_json::from_str::<Resp>(resp.as_str()) {
+    // async fn parsing<T>(resp: String) -> ReqResult<T> {
+    //     match serde_json::from_str::<T>(resp.as_str()) {
+    //         Ok(data) => Ok(data),
+    //         Err(er) => Err(Error::JsonErr {
+    //             details: er.to_string(),
+    //         }),
+    //     }
+    // }
+
+    async fn parsing<'de, T>(resp: &'de String) -> ReqResult<T>
+    where
+        T: Deserialize<'de>,
+    {
+        match serde_json::from_str::<T>(resp.as_str()) {
             Ok(data) => Ok(data),
             Err(er) => Err(Error::JsonErr {
                 details: er.to_string(),
             }),
         }
     }
+
+    async fn get(&mut self, path: String) -> ReqResult<Self::Output> {
+        let url = format!(
+            "{}{}{}",
+            self.url.clone(),
+            Self::PATH.clone(),
+            path.to_owned()
+        );
+
+        let resp = self.client.get(url.to_owned()).send().await?.text().await?;
+        Ok(resp)
+    }
+
+    // async fn post(
+    //     &mut self,
+    //     uri: String,
+    //     header: HashMap<String, String>,
+    //     body: HashMap<String, Value>,
+    // ) -> ReqResult {
+    //     let url = format!(
+    //         "{}{}{}",
+    //         self.url.clone(),
+    //         Self::PATH.clone(),
+    //         uri.to_owned()
+    //     );
+
+    //     // parsing body
+    //     let re_body = json!(body);
+
+    //     // parsing header
+    //     let headers = header::HeaderMap::new();
+    //     let resp = self
+    //         .client
+    //         .post(url.to_owned())
+    //         .body(re_body.to_string())
+    //         .headers(headers)
+    //         .send()
+    //         .await?
+    //         .text()
+    //         .await?;
+
+    //     match serde_json::from_str::<Resp>(resp.as_str()) {
+    //         Ok(data) => Ok(data),
+    //         Err(er) => Err(Error::JsonErr {
+    //             details: er.to_string(),
+    //         }),
+    //     }
+    // }
 }
--- src/rq.rs
+++ src/rq.rs
@@ -1,18 +1,33 @@
 use super::errors::Error;
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::collections::HashMap;
-pub type ReqResult = Result<Resp, Error>;
+pub type ReqResult<T> = Result<T, Error>;
 
 #[async_trait]
 pub trait Request {
+    type Output;
     const URL: &'static str = "http://localhost:4000";
     const PATH: &'static str;
+    // type Output;
     fn new(url: String) -> Self
     where
         Self: Sized;
-    async fn get(&mut self, path: String) -> ReqResult;
-    // fn post(&mut self, body: String) -> ReqResult;
+    async fn get(&mut self, path: String) -> ReqResult<Self::Output>;
+    // async fn get<'de, T>(&mut self, path: &'de String) -> ReqResult<T>
+    // where
+    //     T: Deserialize<'de>;
+
+    async fn parsing<'de, T>(resp: &'de String) -> ReqResult<T>
+    where
+        T: Deserialize<'de>;
+    // async fn post<T>(
+    //     &mut self,
+    //     url: String,
+    //     header: HashMap<String, String>,
+    //     body: HashMap<String, Value>,
+    // ) -> ReqResult<T>;
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
